b821a81a1faef44a8f35273ef88d3c71018b16b3
Joie Zhang <joie@princeton.edu>
2023-06-28 07:30:51 -0700

    brieflyModels doc

---
+++ server/models/brieflyModels.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI = 'mongodb+srv://brieflyadmin:uEObdq1S2Na9Qq2f@briefly.gdsddmd.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(MONGO_URI, {
+  // options for the connect method to parse the URI
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // sets the name of the DB that our collections are part of
+  dbName: 'briefly'
+})
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch(err => console.log(err));
+
+const Schema = mongoose.Schema;
+
+// sets a schema for the 'articles' collection
+const articlesSchema = new Schema({
+  title: { type: String, required: true },
+  publishedOn: { type: Date, required: true },
+  createdAt: { type: Date, default: Date.now },
+  text: { type: String, required: true },
+  summary: { type: String, default: 'Not yet summarized' }
+})
+
+// create a model for the 'articles' collection to be exported
+const Articles = mongoose.model('article', articlesSchema);
+
+// export models for use in the controller
+module.exports = {
+  Articles,
+};
---
+++ server/server.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const express = require('express');
+
+// Connect controllers
+// const userController = require('./controllers/userController');
+// Connect routers
+// const apiRouter = require('./routes/api');
+
+const PORT = 3000;
+
+const app = express();
+
+/**
+ * handle parsing request body
+ */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/**
+ * handle requests for static files
+ */
+app.use(express.static(path.resolve(__dirname, '../client')));
+
+/**
+ * define route handlers
+ */
+// app.use('/api', apiRouter);
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.status(404).send('Page not found; please try again.'));
+
+/**
+ * express error handler
+ * @see https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
+ */
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+/**
+ * start server
+ */
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
+
+module.exports = app;
